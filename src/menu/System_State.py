72c045b16ff9f8f85af50db26f11ca9e8633b2da
Farid Kopzhassarov <kopzhasarov.farid@gmail.com>
2019-04-10 00:44:49 +0300

    Delete System_State.py

--- src/menu/System_State.py
+++
@@ -1,9 +0,0 @@
-class State:
-    def __init__(self):
-        self.working = False
-        self.volume = 0;
-        self.enable_advice = True;
-        self.enable_healthcare = True;
-
-    def info(self):
-        print("System_state:\nWorking: {},\n Volume: {}, \n Advice: {}, \n healthCare: {}, \n".format(self.working, self.volume, self.enable_advice, self.enable_healthcare))
