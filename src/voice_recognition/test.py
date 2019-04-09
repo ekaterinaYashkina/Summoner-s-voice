7edd84645aff307943a01c92bd7a629130881ad7
Amir Nazyrov <senteced210@gmail.com>
2019-04-09 19:14:40 +0300

    create index

--- src/voice_recognition/test.py
+++ src/voice_recognition/test.py
@@ -1,11 +1,39 @@
 import time
 import speech_recognition as sr
 import sys
+import json
+import textdistance
 
 from PyQt5.QtCore import QTimer, QThread
 from PyQt5 import QtWidgets
 
 from info import *
+
+
+class MyIndex(object):
+
+    def __init__(self):
+        with open('phrases.json') as f:
+            self.index = dict(json.load(f))
+
+    def find_most_similar(self, query):
+
+        answer_best_score = dict()
+
+        for phrase in self.index:
+            answer_best_score[phrase] = textdistance.editex.normalized_similarity(phrase, query)
+            for paraphrase in self.index[phrase]["paraphrases"]:
+                v = textdistance.editex.normalized_similarity(paraphrase, query)
+                if v > answer_best_score[phrase]:
+                    answer_best_score[phrase] = v
+
+        result = ''
+        result_v = -1
+        for k in answer_best_score:
+            if answer_best_score[k] > result_v:
+                result = k
+                result_v = answer_best_score[k]
+        return {"name_and_skill": result, "duration": self.index[result]['duration']}
 
 
 class MyThread(QThread):
