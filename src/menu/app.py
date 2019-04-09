23380631b852e23e34e7718e997d434bbb12cbc4
Farid Kopzhassarov <kopzhasarov.farid@gmail.com>
2019-04-10 00:46:58 +0300

    Create app.py

---
+++ src/menu/app.py
@@ -0,0 +1,18 @@
+import sys
+
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication
+
+from views import AppView
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app_view = AppView()
+    app_view.setObjectName('AppView')
+    flags = QtCore.Qt.WindowFlags(
+        QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.SplashScreen
+    )
+    app_view.setWindowFlags(flags)
+    app_view.setStyleSheet('#AppView { border-image: url(res/border.png); }')
+    app_view.show()
+    sys.exit(app.exec_())
