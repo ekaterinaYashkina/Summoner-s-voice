180e6d5dfb5fc5dcf9bfa3bce03752b746f129ed
Farid Kopzhassarov <kopzhasarov.farid@gmail.com>
2019-04-10 18:11:53 +0300

    Update views.py

--- src/menu/views.py
+++ src/menu/views.py
@@ -68,6 +68,7 @@ class AppView(QMainWindow):
         elif key == 'volume':
             obj.volume.setValue(value)
             obj.volume_value.setText(str(value))
+        print(self.app_state)
 
     def switch_active(self, obj, state):
         obj.activate.setStyleSheet('border-image: url(res/' + \
@@ -84,6 +85,7 @@ class AppView(QMainWindow):
                           ('enabled' if state else 'disabled') + '.png);' + \
                           'color: ' + ('green;' if state else 'red;'))
 
+    # Widget getters
     def get_start(self):
         start = StartWidget()
 
@@ -95,34 +97,39 @@ class AppView(QMainWindow):
         about = AboutWidget()
         about.to_menu.setDefault(True)
 
-        about.to_main.clicked.connect(lambda: self.switch_layout(0))
+        about.to_menu.clicked.connect(lambda: self.switch_layout(0))
         about.to_gameinfo.clicked.connect(lambda: self.switch_layout(2))
+        about.to_main.clicked.connect(lambda: self.switch_layout(0))
 
         return about
 
     def get_main(self):
         main = MainWidget()
         main.to_menu.setDefault(True)
-        self.switch_state('active', self.app_state['active'], main)
+        self.switch_active(main, self.app_state['active'])
 
         main.activate.clicked.connect(lambda: self.switch_state('active', obj=main))
         main.status.clicked.connect(lambda: self.switch_state('active', obj=main))
+
+        main.to_gameinfo.clicked.connect(lambda: self.switch_layout(2))
         main.to_about.clicked.connect(lambda: self.switch_layout(-1))
         main.to_settings.clicked.connect(lambda: self.switch_layout(1))
-        main.to_gameinfo.clicked.connect(lambda: self.switch_layout(2))
 
         return main
 
     def get_settings(self):
         settings = SettingsWidget()
         settings.to_menu.setDefault(True)
-        self.switch_state('voice_stats', self.app_state['voice_stats'], settings)
-        self.switch_state('health_care', self.app_state['health_care'], settings)
-        self.switch_state('volume', self.app_state['volume'], settings)
+        self.switch_button(settings.voice_stats, self.app_state['voice_stats'])
+        self.switch_button(settings.health_care, self.app_state['health_care'])
+        settings.volume.setValue(self.app_state['volume'])
+        settings.volume_value.setText(str(self.app_state['volume']))
 
         settings.voice_stats.clicked.connect(lambda: self.switch_state('voice_stats', obj=settings))
         settings.health_care.clicked.connect(lambda: self.switch_state('health_care', obj=settings))
         settings.volume.valueChanged.connect(lambda: self.switch_state('volume', settings.volume.value(), settings))
+
+        settings.to_menu.clicked.connect(lambda: self.switch_layout(0))
         settings.to_main.clicked.connect(lambda: self.switch_layout(0))
         settings.to_gameinfo.clicked.connect(lambda: self.switch_layout(2))
 
@@ -136,6 +143,7 @@ class AppView(QMainWindow):
 
         return gameinfo
 
+    # Make the window draggable
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
             self.press_pos = event.pos()  # remember starting position
